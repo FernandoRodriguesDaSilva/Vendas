c0c9376be6ca5ea30d8ce57c8a4c8c3ec91d4bbd
FernandoRodriguesDaSilva <fernandinhosns@hotmail.com>
2018-10-15 23:42:32 -0300

    Atualizado

--- pdo/connection.php
+++ pdo/connection.php
@@ -12,13 +12,50 @@ try {
 
 	while ($row = $stmt->fetch()) {
 		echo "<p>";
-		 echo $row['idaluno']. ', ';
-		 echo $row['nome']. ', ';
-		 echo $row['idade']. ', ';
-		 echo $row['cidade']. ', ';
-		 echo "</p>";
+		echo $row['idaluno']. ', ';
+		echo $row['nome']. ', ';
+		echo $row['idade']. ', ';
+		echo $row['cidade']. ', ';
+		echo "</p>";
 	}
+
+// Usando outra maneira de inserir no banco 
+	echo "<hr>";
+
+
+	$alunos = [
+		['nome'=>'Patrícia','idade'=>30,'cidade'=>'Fortaleza'],
+		['nome'=>'Pablo','idade'=>10,'cidade'=>'Belo Horizonte']
+	];
+
+	$sql = "INSERT INTO aluno (nome,idade,cidade) VALUES (:nome, :idade, :cidade)";
+
+	$stmt = $conectar->prepare($sql);
+	foreach ($alunos as $a) {
+		$stmt->execute($a);
+	}
+
+/// Obtendo todos os Registros Pablo
+	echo "<p> Imprime todos os pablos </p>";
+	$sql = "SELECT * FROM aluno WHERE nome = ?";
+	$stmt = $conectar->prepare($sql);
+	$stmt->execute(['Pablo']);
+	while ($row = $stmt->fetch()) {
+		echo "<p>";
+		echo $row['idaluno']. ', ';
+		echo $row['nome']. ', ';
+		echo $row['idade']. ', ';
+		echo $row['cidade']. ', ';
+		echo "</p>";
+	}
+
+
+
+
+
 }
+
+
 
 catch (PDOException $e){
 	echo "Erro: ". $e->getMessage();
